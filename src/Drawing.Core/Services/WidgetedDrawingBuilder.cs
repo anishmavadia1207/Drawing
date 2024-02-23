3ce8c26153da19a179eaa5d40153943a22ab9158
Anish Mavadia <anishmavadia1910@gmail.com>
2024-02-23 21:27:26 +0000

    Add widgeted drawing builder implementation

---
+++ src/Drawing.Core/Services/WidgetedDrawingBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Drawing.Abstractions.Models;
+using Drawing.Abstractions.Services;
+
+namespace Drawing.Core.Services;
+public class WidgetedDrawingBuilder : IWidgetedDrawingBuilder
+{
+    internal readonly List<Widget> _widgets = [];
+
+    /// <inheritdoc/>
+    public IWidgetedDrawingBuilder AddWidget(Widget widget)
+    {
+        _widgets.Add(widget);
+        return this;
+    }
+
+    /// <inheritdoc/>
+    public WidgetedDrawing Build()
+    {
+        return new WidgetedDrawing(_widgets);
+    }
+}
---
+++ tests/Drawing.Core.Test.Unit/Services/WidgetedDrawingBuilderShould.cs
@@ -0,0 +1,40 @@
+﻿using Drawing.Abstractions.Models;
+using Drawing.Abstractions.Models.Shapes;
+using Drawing.Core.Services;
+
+using FluentAssertions;
+
+public class WidgetedDrawingBuilderTests
+{
+    [Fact]
+    public void Update_Collection_When_AddingWidget()
+    {
+        var builder = new WidgetedDrawingBuilder();
+        var widget = new Widget(new Position(10, 20), new Circle(5));
+
+        builder.AddWidget(widget);
+
+        builder._widgets.Should().Contain(widget);
+    }
+
+    [Fact]
+    public void Return_BuiltWidgetedDrawing_When_BuildingCollection()
+    {
+        var builder = new WidgetedDrawingBuilder();
+        var widgets = new List<Widget>
+        {
+            new Widget(new Position(10, 20), new Circle(5)),
+            new Widget(new Position(30, 40), new Rectangle(6, 8))
+        };
+
+        foreach (var widget in widgets)
+        {
+            builder.AddWidget(widget);
+        }
+
+        // Act
+        var widgetedDrawing = builder.Build();
+
+        widgetedDrawing.Widgets.Should().BeEquivalentTo(widgets);
+    }
+}
