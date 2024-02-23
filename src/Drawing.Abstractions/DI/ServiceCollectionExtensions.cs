64b2f9b6d0abf9a0054461cc1794a7ebd6d2dbb9
Anish Mavadia <anishmavadia1910@gmail.com>
2024-02-23 23:11:52 +0000

    Add DI methods for implementations

---
+++ src/Drawing.Abstractions/DI/ServiceCollectionExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Drawing.Abstractions.Services;
+using Drawing.Abstractions.Services.Builders;
+using Drawing.Abstractions.Services.Renderer;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Drawing.Abstractions.DI;
+
+/// <summary>
+/// Extension methods for adding drawing-related services to the <see cref="IServiceCollection"/>.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds drawing-related services to the <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <typeparam name="TDrawingService">The type of drawing service to add.</typeparam>
+    /// <typeparam name="TIWidgetDrawingBuilder">The type of widget drawing builder to add.</typeparam>
+    /// <typeparam name="TWidgetRenderer">The type of widget renderer to add.</typeparam>
+    /// <param name="this">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <param name="serviceLifetime">The service lifetime to use for the added services (default is <see cref="ServiceLifetime.Scoped"/>).</param>
+    /// <returns>The modified <see cref="IServiceCollection"/> instance.</returns>
+    /// <remarks>
+    /// This method registers the specified drawing service, widget builder, and widget renderer 
+    /// types with the specified service lifetime in the <paramref name="this"/> service collection.
+    /// </remarks>
+    public static IServiceCollection AddDrawingServices<TDrawingService, TIWidgetDrawingBuilder, TWidgetRenderer>(
+        this IServiceCollection @this,
+        ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        where TDrawingService : class, IDrawingService
+        where TIWidgetDrawingBuilder : class, IWidgetDrawingBuilder
+        where TWidgetRenderer : class, IWidgetRenderer
+    {
+        @this.Add(
+            new ServiceDescriptor(
+                typeof(IDrawingService),
+                typeof(TDrawingService),
+                serviceLifetime));
+
+        @this.Add(
+            new ServiceDescriptor(
+                typeof(IWidgetDrawingBuilder),
+                typeof(TIWidgetDrawingBuilder),
+                serviceLifetime));
+
+        @this.Add(
+            new ServiceDescriptor(
+                typeof(IWidgetRenderer),
+                typeof(TWidgetRenderer),
+                serviceLifetime));
+
+        return @this;
+    }
+}
---
+++ src/Drawing.Core/DI/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Drawing.Abstractions.DI;
+using Drawing.Abstractions.Services.Strategies;
+using Drawing.Core.Services;
+using Drawing.Core.Services.Builders;
+using Drawing.Core.Services.Renderer;
+using Drawing.Core.Services.Strategies;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Drawing.Core.DI;
+
+/// <summary>
+/// Extension methods for adding drawing-related services to the <see cref="IServiceCollection"/>.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCoreDrawingServices(
+        this IServiceCollection @this,
+        ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+    {
+        @this.AddDrawingServices<DrawingService, WidgetDrawingBuilder, WidgetRenderer>(serviceLifetime);
+        @this.AddSingleton<IRenderStrategy, ShapeTypeStrategy>();
+        @this.AddSingleton<IRenderStrategy, PositionStrategy>();
+        @this.AddSingleton<IRenderStrategy, CircularShapeStrategy>();
+        @this.AddSingleton<IRenderStrategy, RectangularShapeStrategy>();
+
+        return @this;
+    }
+}
