6e9f5017e5d12bf7853cc7b38db324b36b483b82
Anish Mavadia <anishmavadia1910@gmail.com>
2024-02-24 00:09:06 +0000

    Add tests for widget renderer

--- tests/Drawing.Core.Test.Unit/Services/DrawingServiceShould.cs
+++ tests/Drawing.Core.Test.Unit/Services/DrawingServiceShould.cs
@@ -1,4 +1,33 @@
-﻿namespace Drawing.Core.Test.Unit.Services;
+﻿using Drawing.Abstractions.Models;
+using Drawing.Abstractions.Services.Renderer;
+using Drawing.Core.Services;
+using Drawing.Test.Generators;
+
+using FluentAssertions.Execution;
+
+using NSubstitute;
+
+namespace Drawing.Core.Test.Unit.Services;
 public class DrawingServiceTests
 {
+    private readonly DrawingService _service;
+    private readonly IWidgetRenderer _rendererSubstitute;
+    public DrawingServiceTests()
+    {
+        _rendererSubstitute = Substitute.For<IWidgetRenderer>();
+        _service = new DrawingService(_rendererSubstitute);
+    }
+
+    [Fact]
+    public void Call_RendererForEachWidget_When_Rendering()
+    {
+        var widgets = TestGenerator.WidgetDrawingGenerator().Generate();
+
+        _ = _service.Draw(widgets);
+
+        using var scope = new AssertionScope();
+        _rendererSubstitute
+            .Received(widgets.Widgets.Count())
+            .Render(Arg.Any<Widget>());
+    }
 }
---
+++ tests/Drawing.Core.Test.Unit/Services/Renderer/WidgetRendererShould.cs
@@ -0,0 +1,63 @@
+﻿using Drawing.Abstractions.Models;
+using Drawing.Abstractions.Services.Strategies;
+using Drawing.Core.Services.Renderer;
+using Drawing.Test.Generators;
+
+using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace Drawing.Core.Test.Unit.Services.Renderer;
+public class WidgetRendererShould
+{
+    private readonly WidgetRenderer _renderer;
+    private readonly IEnumerable<IRenderStrategy> _strategies;
+    public WidgetRendererShould()
+    {
+        _strategies =
+        [
+            new FirstStrategy(),
+            new SecondStrategy(),
+            new ThirdStrategy()
+        ];
+
+        _renderer = new WidgetRenderer(_strategies);
+    }
+
+    [Fact]
+    public void Use_StrategyOrder_When_Rendering()
+    {
+        var widget = TestGenerator.WidgetGenerator().Generate();
+
+        var rendered = _renderer.Render(widget);
+
+        using var scope = new AssertionScope();
+        rendered.Should().Be("1 2 3");
+    }
+
+    [Fact]
+    public void Trim_ExcessWhiteSpace_When_Rendering()
+    {
+        var widget = TestGenerator.WidgetGenerator().Generate();
+
+        var rendered = _renderer.Render(widget);
+
+        using var scope = new AssertionScope();
+        rendered.Should().NotEndWith(" ");
+        rendered.Should().NotStartWith(" ");
+    }
+
+    private class FirstStrategy : IRenderStrategy
+    {
+        public string GetStrategyResult(Widget widget) => "   1";
+    }
+
+    private class SecondStrategy : IRenderStrategy
+    {
+        public string GetStrategyResult(Widget widget) => "2";
+    }
+
+    private class ThirdStrategy : IRenderStrategy
+    {
+        public string GetStrategyResult(Widget widget) => "3    ";
+    }
+}
