c2f662e38ae2a9bab81119a30fffa33a1dfee825
Anish Mavadia <anishmavadia1910@gmail.com>
2024-02-29 11:30:34 +0000

    Create intial DI method for kafka producers

---
+++ src/Drawing.Producers/Constants/KafkaKeys.cs
@@ -0,0 +1,12 @@
+﻿namespace Drawing.Producers.Constants;
+
+/// <summary>
+/// Constants for Kafka keys
+/// </summary>
+public static class KafkaKeys
+{
+    /// <summary>
+    /// The topic name for shapes
+    /// </summary>
+    public const string ShapeTopicName = "Shapes";
+}
---
+++ src/Drawing.Producers/DI/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Drawing.Producers.Constants;
+
+using KafkaFlow;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Drawing.Producers.DI;
+
+/// <summary>
+/// Extension methods for IServiceCollection for adding Kafka producers related to drawing
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds Kafka producers for drawing
+    /// </summary>
+    /// <param name="kafkaClusterUrl">The Kafka cluster URL</param>
+    /// <param name="kafkaUsername">The Kafka username</param>
+    /// <param name="kafkaPassword">The Kafka password</param>
+    /// <returns>The modified IServiceCollection</returns>
+    public static IServiceCollection AddDrawingKafkaProducers(
+        this IServiceCollection @this,
+        string kafkaClusterUrl,
+        string kafkaUsername,
+        string kafkaPassword) =>
+        @this.AddKafka(kafka =>
+            kafka.AddCluster(cluster => cluster
+                .WithBrokers([kafkaClusterUrl])
+                .WithSecurityInformation(security =>
+                {
+                    security.SaslUsername = kafkaUsername;
+                    security.SaslPassword = kafkaPassword;
+                })
+                .CreateTopicIfNotExists(KafkaKeys.ShapeTopicName, 1, 1)));
+}
